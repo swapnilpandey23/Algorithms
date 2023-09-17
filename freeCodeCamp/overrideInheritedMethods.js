d1dfd2a52689bf33691124c2e0cb3f4fb1e43ed2
Swapnil Pandey <123621903+swapnilpandey23@users.noreply.github.com>
2023-09-17 08:53:37 +0000

    Overriding inherited methods from Supertype

---
+++ freeCodeCamp/overrideInheritedMethods.js
@@ -0,0 +1,18 @@
+//Kindly refer to inheritFromSupertype.js for inheritance related comments.
+
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Overriding Supertype's inherited method.
+Penguin.prototype.fly = function(){
+  return "Alas, this is a flightless bird.";
+}
+
+
+let penguin = new Penguin();
+console.log(penguin.fly());
